3d92a579b6565a173467c1f959aa2356f5577f6e
Loawolf <80107270+Loawolf@users.noreply.github.com>
2023-01-30 10:22:33 +0300

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
 print('hello world!')
+a=input('Выбери опцию(+;-;/;*;А;В)')
+if a=='А':
+	print('A')
+if a=='В':
+	print('B')
+if a=='+':
+	print(2+2)
+if a=='-':
+	print(2-2)
+if a=='*':	
+	print(2*2)
+if a=='/':
+	print(2/2)
+input('конец')
